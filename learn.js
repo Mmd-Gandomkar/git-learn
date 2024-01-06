aefb556b3d0dd59fd0987075a52f9fb10b0fa6f3
Mmd-Gandomkar <dawrdesawr@gmail.com>
2024-01-07 02:05:06 +0330

    tamrin_class_jalase4

--- learn.js
+++ learn.js
@@ -11,3 +11,32 @@ console.log(jamotaf('add',[20,30,5,2] ));
 console.log(jamotaf('dev',[20,30,5,2] ));
 console.log(jamotaf('mult',[20,30,5,2] ));
 console.log(jamotaf('min',[20,30,5,2] ));
+
+const Mohammad = {
+    firstName: "Mohammad",
+    lastName: "Gandom kar",
+    age: 19,
+    job: "teacher" ,
+    ismarried: false,
+    hobbies: ["Driving" , "Riding" , "reading"] ,
+
+    drive : function () {
+        return 'I CAN DRIVING' ;
+    },
+    ride : function () {
+        return ' I CAN RIDING A BICYCLE' ;
+    },
+    read : function () {
+        return ' I CAN READING';
+    },
+
+    
+}
+
+console.log(Mohammad.drive())
+console.log(Mohammad.ride())
+console.log(Mohammad.read())
+console.log(Mohammad.firstName)
+console.log(Mohammad.lastName)
+console.log(Mohammad.hobbies)
+
